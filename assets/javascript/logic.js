7f2b6c751cfb5175d63701f7d29991cfc6f2b8c4
graygabrielle <gray.atkinson.adams@gmail.com>
2019-03-17 23:46:30 -0700

    added variables to check current time against first train time and set firsttrainalready to true or false

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,7 +2,6 @@ var trains = [{Name: "Dublin", Destination: "San Fransisco", Frequency: 25, Next
 
 var i = 0;
 
-console.log(trains[i].Name);
 
 //appends your item to your chosen tag/element
 
@@ -14,12 +13,60 @@ function appendFunc(dest, item){
 //pushes user input as an object into trains array, and posts it on html page
 function addTrain() {
 
+    //useful variables
+    let currentTime = moment().format("HH:mm");
+    let currentHoursAndMinutes = currentTime.split(":");
+    let currentHour = parseInt(currentHoursAndMinutes[0]);
+    let currentMinute = parseInt(currentHoursAndMinutes[1]);
+    // console.log(currentHour);
+    // console.log(typeof currentHour);
+    // console.log(currentMinute);
+    // console.log(typeof currentMinute);
+
+    let firstTrain = $("#first-train-time").val().trim();
+    let firstHoursAndMinutes = firstTrain.split(":");
+    let firstHour = parseInt(firstHoursAndMinutes[0]);
+    let firstMinute = parseInt(firstHoursAndMinutes[1]);
+    // console.log(firstHour);
+    // console.log(typeof firstHour);
+    // console.log(firstMinute);
+    // console.log(typeof firstMinute);
+
+    let firstTrainAlready = false;
+    let rightNow = false;
+
+    if (currentHour>firstHour) {
+        firstTrainAlready = true;
+    }
+    else if (currentHour===firstHour) {
+        if(currentMinute>firstMinute){
+            firstTrainAlready = true;
+        }
+        else if (currentMinute===firstMinute){
+            rightNow = true;
+        }
+        else {
+            firstTrainAlready = false;
+        }
+    }
+    else {
+        firstTrainAlready = false;
+    }
+
+    console.log(firstTrainAlready);
+    console.log(rightNow);
+
+
+
+
+    //variables that will be printed to the table
     let name = $("#train-name").val().trim();
     let destination = $("#destination").val().trim();
-    let firstTrain = $("#first-train-time").val().trim();
     let frequency = $("#frequency").val().trim();
-    let nextArrival = "moment.js";
-    let minutesAway = "moment.js";
+    let nextArrival = "moment";
+    let minutesAway = "moment";
+
+
 
     let newTrain = {
         Name: name,
@@ -73,4 +120,7 @@ $(".submit").on("click", function(event){
     addTrain();
     clearInput();
 })
+
+
+
 
